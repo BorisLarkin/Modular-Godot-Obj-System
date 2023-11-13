8831897afc321119113526f7ecbbe69506906f3f
ITManiac <borislarkin18@mail.ru>
2023-11-13 20:20:41 +0300

    wasd

--- Godot4_Game1/Player.cs
+++ Godot4_Game1/Player.cs
@@ -9,7 +9,7 @@ public partial class Player : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
+		// As good practice, you should replace ui actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
