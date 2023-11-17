57848ffed36668b20f0897dc797542e3a6c5680b
Borizzler <borislarkin18@mail.ru>
2023-11-17 09:40:02 +0300

    timeouts

--- Godot4_Game1/abilities/Ability.cs
+++ Godot4_Game1/abilities/Ability.cs
@@ -1,28 +1,47 @@
 using Godot;
 using System;
 
-public abstract partial class Ability : Timer
+public abstract partial class Ability
 {
 	public float CD {get; set;}
 	public float use_time{get; set;}
 	public float cost {get; set;}
 	public Timer useTimer;
+	public Timer CDTimer;
+	protected bool CanUse;
 	protected Ability(){
 		CD = 0.5f;
 		use_time = 0.2f;
 		cost = 0.0f;
-		useTimer = GetNode<Timer>("useTimer");
+		useTimer = new Timer();
+		CDTimer = new Timer();
+		CanUse = true;
 	}
 	public abstract void Use(entity Obj);
+	
 	protected Ability(Ability Obj){
 		if (this != Obj){
 			this.CD = Obj.CD;
 			this.use_time = Obj.use_time;
 			this.cost = Obj.cost;
 			this.useTimer = new Timer();
-
+			this.CDTimer = new Timer();
+			useTimer.WaitTime = use_time;
+			CDTimer.WaitTime = CD;
+			CanUse = true;
 		}
 	}
 	
 	public abstract object Clone();
+
+	protected void _on_use_timer_timeout()
+    {
+        CanUse = false;
+        CDTimer.Start();
+    }
+
+    protected void _on_cd_timer_timeout()
+    {
+        CanUse = true;
+    }
 }
--- Godot4_Game1/abilities/dash/Dash.cs
+++ Godot4_Game1/abilities/dash/Dash.cs
@@ -34,5 +34,4 @@ public partial class Dash : Ability
     {
        return new Dash(this);
     }
-    
 }
