6d72584c0ede88d9c2fe2b41427a431becc22190
Borizzler <borislarkin18@mail.ru>
2023-11-20 10:58:52 +0300

    preps for locating timer()

--- Godot4Game1/Ability/Ability.cs
+++ Godot4Game1/Ability/Ability.cs
@@ -12,10 +12,15 @@ public partial class Ability : Node2D
 	
 	protected virtual void Use(entity Obj){}
 	
+	public override void _Ready()
+	{
+    	useTimer = GetNode<Timer>("useTimer");
+		CDTimer = GetNode<Timer>("CDTimer");
+	}
 	public void UseAbility(entity Obj)
 	{
 		if (CanUse){
-			//useTimer.Start();
+			useTimer.Start();
 			GD.Print("dashed ", CD);
 			Use(Obj);
 		}
@@ -23,12 +28,11 @@ public partial class Ability : Node2D
 
 	protected Ability(Ability Obj){
 		if (this != Obj){
-			GD.Print("Ability copy constr");
 			this.CD = Obj.CD;
 			this.use_time = Obj.use_time;
 			this.cost = Obj.cost;
-			this.useTimer = new Timer();
-			this.CDTimer = new Timer();
+			//this.useTimer = new Timer();
+			//this.CDTimer = new Timer();
 			useTimer.WaitTime = use_time;
 			CDTimer.WaitTime = CD;
 			useTimer.Autostart = true;
@@ -41,12 +45,14 @@ public partial class Ability : Node2D
 		CD = cd;
 		use_time = uset;
 		cost = ct;
-		useTimer = new Timer();
-		CDTimer = new Timer();
+		//useTimer = new Timer();
+		//CDTimer = new Timer();
+		//useTimer = GetNode("res://Ability/Ability.tscn").GetNode<Timer>("useTimer");
+		//CDTimer = GetNode<Timer>("CDTimer");
 		CDTimer.WaitTime = CD;
 		useTimer.WaitTime = use_time;
-		useTimer.Autostart = true;
-		CDTimer.Autostart = true;
+		//useTimer.Autostart = true;
+		//CDTimer.Autostart = true;
 		CanUse = true;
 	}
 	protected void _on_use_timer_timeout()
