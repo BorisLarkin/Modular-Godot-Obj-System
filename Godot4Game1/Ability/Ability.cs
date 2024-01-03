29e29755e44b1878b4d9d446733bb37fd17a764d
ITManiac <borislarkin18@mail.ru>
2024-01-03 16:10:36 +0300

    go off modular

--- Godot4Game1/Ability/Ability.cs
+++ Godot4Game1/Ability/Ability.cs
@@ -9,65 +9,59 @@ public unsafe partial class Ability : Node2D, ICloneable
 	public float cost;
 	protected Timer useTimer;
 	protected Timer CDTimer;
-	
-	protected Godot.Variant CanUse;
+	protected virtual void set_canuse_true(){
+		SetMeta("CanUse",true);
+		return;
+	}
+	protected virtual void set_canuse_false(){
+		SetMeta("CanUse",false);
+		return;
+	}
+	protected virtual bool get_use_state(){
+		return (bool)GetMeta("CanUse");
+	}
 	public object Clone()
 	{
 		return this.MemberwiseClone();
 	}
-	protected virtual void Use(entity Obj){	}
-	
+	protected virtual void Use(entity Obj){}
+	public virtual void UseAbility(entity Obj){}
 	void _on_ready()
 	{
 		useTimer = GetNode<Timer>("useTimer");
 		CDTimer = GetNode<Timer>("CDTimer");
 		useTimer.WaitTime = use_time;
 		CDTimer.WaitTime = CD;
-
 	}
 	
-	public void UseAbility(entity Obj)
-	{
-		GD.Print(CanUse);
-		if ((bool)CanUse == true){
-			Use(Obj);
-			SetMeta("CanUse",false);
-			useTimer.Start();
-		}
-	}
-
 	protected Ability(Ability Obj){
 		if (this != Obj){
 			this.CD = Obj.CD;
 			this.use_time = Obj.use_time;
 			this.cost = Obj.cost;
-			CanUse = Obj.CanUse;
 		}
 	}
 	protected Ability(float cd, float uset, float ct){
 		CD = cd;
 		use_time = uset;
 		cost = ct;
-		CanUse = GetMeta("CanUse");
-		SetMeta("CanUse",true);
+		set_canuse_true();
 	}
 	protected void _on_use_timer_timeout()
 	{
-		GD.Print("use_t timeout", CanUse,CanUse);
+		GD.Print("use_t timeout", get_use_state());
 		CDTimer.Start();
 	}
 
 	protected void _on_cd_timer_timeout()
 	{
-		SetMeta("CanUse",true);
-		GD.Print("cd_t timeout", CanUse);
+		set_canuse_true();
+		GD.Print("cd_t timeout", get_use_state());
 	}
 	public Ability()
 	{
 		CD=1.0f;
 		use_time=0.5f;
 		cost = 0;
-		CanUse = GetMeta("CanUse");
-		SetMeta("CanUse",true);
 	}
 }
--- Godot4Game1/Ability/Dash/Dash.cs
+++ Godot4Game1/Ability/Dash/Dash.cs
@@ -4,7 +4,17 @@ public unsafe partial class Dash : Ability
 {
 	public float dash_speed;
 	public bool ghost_on;
-
+	protected override void set_canuse_true(){
+		SetMeta("CanUse",true);
+		return;
+	}
+	protected override void set_canuse_false(){
+		SetMeta("CanUse",false);
+		return;
+	}
+	protected override bool get_use_state(){
+		return (bool)GetMeta("CanUse");
+	}
 	protected override void Use(entity Obj)
 	{
 		Obj.velocity.X = Obj.direction.X * dash_speed;
@@ -20,8 +30,6 @@ public unsafe partial class Dash : Ability
 	{
 		dash_speed = 600f;
 		ghost_on = false;
-		CanUse = base.CanUse;
-
 	}
 
 	public void set(float cd, float uset, float ct, float dash_spd, bool ghost)
@@ -36,6 +44,16 @@ public unsafe partial class Dash : Ability
 		useTimer.WaitTime = uset;
 		dash_speed = dash_spd;
 		ghost_on = ghost;
-		this.CanUse = base.CanUse;
+		SetMeta("CanUse", true);
+	}
+	
+	public override void UseAbility(entity Obj)
+	{
+		GD.Print(get_use_state());
+		if (get_use_state() == true){
+			Use(Obj);
+			set_canuse_false();
+			useTimer.Start();
+		}
 	}
 }
